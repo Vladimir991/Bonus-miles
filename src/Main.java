f788fc67134274382bf56c43fef2de0eb6059f8c
Vladimir Voskoboev <wiz991@mail.ru>
2023-01-28 16:29:49 +0500

    run program

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 7345;
+        int amount = 20;
+        int bonus = price / amount;
+          System.out.print("Количество бонусных миль: ");
+            System.out.print(bonus);
+    }
+}
